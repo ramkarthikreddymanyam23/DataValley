235facb09a82071ed23aa0338d4ddc2fb1849573
ramkarthikreddymanyam23 <79128024+ramkarthikreddymanyam23@users.noreply.github.com>
2024-03-18 18:10:22 +0530

    Add files via upload

---
+++ assignement3/sound.java
@@ -0,0 +1,36 @@
+package assignement3;
+
+class Animal {
+    void makeSound() {
+        System.out.println("Some generic sound");
+    }
+    
+    void makeSound(String specificSound) {
+        System.out.println(specificSound);
+    }
+}
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof");
+    }
+    
+    void makeSound(int frequency) {
+        System.out.println("Woof at frequency: " + frequency);
+    }
+}
+public class sound {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.makeSound();  
+        animal.makeSound("Meow");  
+
+        dog.makeSound(); 
+        dog.makeSound(100); 
+        
+        animal = new Dog(); 
+        animal.makeSound(); 
+    }
+}
